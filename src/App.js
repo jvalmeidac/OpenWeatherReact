7716b81cc6f58e64d11ceb62bdec73dcd0e1de9b
João Victor <joaovictor.61@hotmail.com>
2020-04-02 17:46:56 -0300

    Desenvolvimento da funcionalidade

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FiSearch } from "react-icons/fi";
+import api from "./services/api";
 
 function App() {
+  const key = "ba11e837904d6c52db0804ff65411068";
+
+  const [city, setCity] = useState("");
+  const [cityName, setCityName] = useState("");
+  const [temp, setTemp] = useState(0);
+  const [tempMin, setTempMin] = useState(0);
+  const [tempMax, setTempMax] = useState(0);
+  const [pressure, setPressure] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const dataFormat = d => {
+    let meses = [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro"
+    ];
+    let dias = [
+      "Domingo",
+      "Segunda",
+      "Terça",
+      "Quarta",
+      "Quinta",
+      "Sexta",
+      "Sábado"
+    ];
+    let dia = dias[d.getDay()];
+    let data = d.getDate();
+    let mes = meses[d.getMonth()];
+    let ano = d.getFullYear();
+    return `${dia}, ${data} ${mes} ${ano}`;
+  };
+
+  async function getDatas() {
+    const response = await api.get(
+      `weather?q=${city}&units=metric&APPID=${key}&lang=pt_br`
+    );
+    const result = response.data;
+    setTemp(result.main.temp);
+    setTempMin(result.main.temp_min);
+    setTempMax(result.main.temp_max);
+    setPressure(result.main.pressure);
+    setHumidity(result.main.humidity);
+    setDescription(result.weather[0].description);
+    setCityName(result.name);
+  }
+
+  useEffect(() => {
+    getDatas();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{cityName}</h1>
+      <div>
+        <input
+          placeholder="Digite aqui o nome da cidade..."
+          value={city}
+          onChange={e => setCity(e.target.value)}
+          target="_black"
+        />
+        <FiSearch size={50} color="#e0e0e6" onClick={() => getDatas()} />
+        <div class="date">{dataFormat(new Date())}</div>
+        <h2>
+          {Math.round(temp)}
+          <span>°</span>
+        </h2>
+        <h6>{description}</h6>
+        <h6>Máxima: {tempMax}</h6>
+        <h6>Mínima: {tempMin}</h6>
+        <h6>Pressão Atmosférica: {pressure}</h6>
+        <h6>Humidade: {humidity}</h6>
+      </div>
     </div>
   );
 }
